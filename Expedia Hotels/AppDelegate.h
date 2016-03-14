97eb08156fea3993c65cdcdf293e7bd4495f1b11
Brian Peterman <bpeterman@expedia.com>
2016-03-14 15:42:41 -0500

    Initial Commit

---
+++ Expedia Hotels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Expedia Hotels
+//
+//  Created by Brian Peterman on 3/14/16.
+//  Copyright © 2016 Expedia Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
